18b6070954e27bd06bd4e4977e6e76ce32e3da84
hayanoY_124 <131837279+sakuj123@users.noreply.github.com>
2024-06-11 15:31:29 +0900

    Hello 문예진

--- 2107/src/App.js
+++ 2107/src/App.js
@@ -1,25 +1,10 @@
-import logo from './와플곰.png';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>2학년 1반 문예진</code>
-        </p>
-        <p>
-          진로희망 : 데이터 엔지니어입니다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+    <div>
+      <Hello name='문예진' age='18'/>
     </div>
   );
 }
---
+++ 2107/src/Hello.jsx
@@ -0,0 +1,11 @@
+const Hello = (props) => {
+    return(
+        <div>
+            {props.name} 
+            {props.age}
+        </div>
+
+    )
+}
+
+export default Hello;
