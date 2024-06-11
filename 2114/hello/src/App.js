3130a5b0092b259ed5ad48037a04cacd4d676b02
Suhwan Lee <133092014+Suhwan623@users.noreply.github.com>
2024-06-11 15:31:35 +0900

    2114 이수환

---
+++ 2114/hello/src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Hello name="이수환" age="18" />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2114/hello/src/Hello.jsx
@@ -0,0 +1,9 @@
+const Hello = (props) => {
+  return (
+    <div>
+      {props.name}
+      {props.age}
+    </div>
+  );
+};
+export default Hello;
