337cda8374943a61ca3bff3dd359f303cbb7cf9d
YeHyun Byeon <oopptteh@gmail.com>
2024-06-11 15:26:39 +0900

    Add files via upload

---
+++ 2110/count/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Viewer from './Viewer';
+import Controller from './Controller';
+import './App.css';
+
+function App() {
+  //count라는 상태 만들기 및 값 초기화
+  const [count, setCount] = useState(0);
+
+  const handleSetCount = ( value ) => {
+    setCount(count + value)
+  }
+
+
+  return (
+    <div className='App'>
+      <section>
+        <Viewer count={count}/>
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount}/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
