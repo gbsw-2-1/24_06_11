a7d946b6168b697e65f6f53c7a8d24600390fb0a
신수지 <155224169+3suji3@users.noreply.github.com>
2024-06-11 15:26:57 +0900

    2111신수지

---
+++ 2111/hello/src/App.js
@@ -0,0 +1,11 @@
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      <Hello name='suji' age='18'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2111/hello/src/Hello.jsx
@@ -0,0 +1,10 @@
+const Hello = (props) => {
+  return (
+    <div>
+      {props.name}
+      {props.age}
+    </div>
+  )
+}
+
+export default Hello;
