b0edcba24ff3d781b4636f8e5f5f1e93e16fbf3c
yoon121317 <133763502+yoon121317@users.noreply.github.com>
2024-06-11 16:27:55 +0900

    Add files via upload

---
+++ 2112/hello/src/App.js
@@ -0,0 +1,12 @@
+import Hello from './Hello';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello name="17 " age="20071213" />
+    </div>
+  );
+}
+
+export default App;
