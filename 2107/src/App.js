200315447c4c99f0d93ec86cd759cbb2e45c8b0d
hayanoY_124 <131837279+sakuj123@users.noreply.github.com>
2024-06-11 15:26:51 +0900

    test 문예진

---
+++ 2107/src/App.js
@@ -0,0 +1,27 @@
+import logo from './와플곰.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>2학년 1반 문예진</code>
+        </p>
+        <p>
+          진로희망 : 데이터 엔지니어입니다.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
